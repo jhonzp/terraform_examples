31779929c7c15b2193b01d497695b3b60a19e118
Jhon Zuñiga <zuni230669>
2021-01-31 19:02:18 -0500

    modules

--- demo-5/vars.tf
+++ demo-5/vars.tf
@@ -2,12 +2,8 @@ variable "AWS_REGION" {
   default = "eu-west-1"
 }
 
-variable "AWS_ACCESS_KEY" { 
-}
-
-
-variable "AWS_SECRET_KEY" { 
-}
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
 
 variable "AMIS" {
   type = map(string)
